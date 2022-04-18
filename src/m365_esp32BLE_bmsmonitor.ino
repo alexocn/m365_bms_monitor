33bc363679ea10a263ed7bc53939643979e37e15
wabmester <39422301+wabmester@users.noreply.github.com>
2022-04-17 20:48:12 -0700

    Bug fix
    Broke the checksum check for XM case

--- src/m365_esp32BLE_bmsmonitor.ino
+++ src/m365_esp32BLE_bmsmonitor.ino
@@ -148,7 +148,7 @@ void parse_31(const byte *pkt) {
 //           LENG/ADDR/READ/CMND/CAPACITY /CHARGE   /CURRENT  /VOLTAGE  /TEMP    */
 //{0x55,0xAA,0x0C,0x25,0x01,0x31,0xDE,0xAD,0xBE,0x00,0x10,0x00,0xFA,0xCE,0x2A,0x2B};
   int off;
-  if (packetType = NB) off = 1;
+  if (packetType == NB) off = 1;
   else off = 0;
   parsed_state.capacity = pkt[off+6] + (pkt[off+7] << 8);
   parsed_state.charge = pkt[off+8] + (pkt[off+9] << 8);
@@ -349,14 +349,14 @@ class MyCallbacks: public BLECharacteristicCallbacks {
       if(rxValue[0]==0x55 && rxValue[1]==0xAA){
         fType = 1;
         reqId = rxValue[5];
-        lenOff = 0;
       }
       else if(rxValue[0]==0x5A && rxValue[1]==0xA5){
         fType = 2;
-        reqId = rxValue[6];
-        lenOff = 1;     
+        reqId = rxValue[6];    
       }
       else return;
+
+      lenOff = (packetType == XM) ? 0 : 1;
 
       //I hijacked some request which are not 1:1, but it seems it works, M365Tools still process responses no matter what it requested
       if(reqId==0x1A) {
